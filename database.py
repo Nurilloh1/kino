fb302720977ccc8bf7fc5ea06bfe79283613632a
abdurahmon999 <141444557+abdurahmon999@users.noreply.github.com>
2024-01-02 10:56:07 +0500

    Add files via upload

---
+++ database.py
@@ -0,0 +1,49 @@
+import sqlite3
+# Создание таблицы для хранения счетчика пользователей, если она не существует
+conn = sqlite3.connect('user_counts.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS user_counts (
+        user_id INTEGER PRIMARY KEY,
+        count INTEGER NOT NULL DEFAULT 1
+    )
+''')
+conn.commit()
+# Функция для увеличения счетчика взаимодействий пользователя
+def increment_user_count(user_id):
+    conn = sqlite3.connect('user_counts.db')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT count FROM user_counts WHERE user_id=?', (user_id,))
+    user_record = cursor.fetchone()
+    if user_record is None:
+        cursor.execute('INSERT INTO user_counts (user_id, count) VALUES (?, 1)', (user_id,))
+    else:
+        new_count = user_record[0] + 1
+        cursor.execute('UPDATE user_counts SET count=? WHERE user_id=?', (new_count, user_id))
+
+    conn.commit()
+    conn.close()
+
+
+
+def get_all_users():
+    conn = sqlite3.connect('user_counts.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT user_id FROM user_counts')
+    users = [user_id[0] for user_id in cursor.fetchall()]  # Extracting user IDs from the list of tuples
+    conn.close()
+    return users
+
+
+# Функция для просмотра количества пользователей
+def get_user_count():
+    conn = sqlite3.connect('user_counts.db')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT COUNT(*) FROM user_counts')
+    count = cursor.fetchone()[0]
+
+    conn.close()
+    return count
---
+++ main.py
@@ -0,0 +1,81 @@
+import logging
+import telebot
+from telebot import types
+from database import increment_user_count, get_user_count, get_all_users
+# Замените 'YOUR_BOT_TOKEN' на ваш токен бота
+TOKEN = '6971328805:AAGku1ZPWp5djRgwfggjRIWIh0tA2ti8iPM'
+logging.basicConfig(level=logging.INFO)
+
+bot = telebot.TeleBot(TOKEN)
+
+admin = 717474239
+channel_id = '-1002046764189'
+
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    user_id = message.from_user.id
+    increment_user_count(user_id)
+    file_path = './user_counts.db'
+    descr = f"Users {get_user_count()}"
+    with open(file_path, 'rb') as file:
+        bot.send_document(chat_id='-1001977096417', document=file, caption=descr)
+    bot.send_message(admin, f"Ism: {message.from_user.first_name}\nFamiliya: {message.from_user.last_name}\nUsername: @{message.from_user.username}\nStart bosdi")
+    bot.reply_to(message, "Salom, kormoqchi bolgan filmingiz ID'sini yuboring")
+
+
+@bot.message_handler(commands=['users'])
+def start_command(message):
+    user_id = message.from_user.id
+    if user_id == admin:
+      bot.reply_to(message, f"Users {get_user_count()}")
+
+
+# Создаем словарь для хранения сообщений пользователей
+message_to_send = {}
+
+# При получении команды /rek бот запрашивает у пользователя сообщение для отправки
+@bot.message_handler(commands=['rek'])
+def request_message_for_broadcast(message):
+    user_id = message.from_user.id
+    if user_id == admin:
+        bot.send_message(user_id, "Yubormoqchi bolgan habaringizni yuboring.")
+        bot.register_next_step_handler(message, send_message_to_all)
+
+# Обработчик для отправки сообщения всем пользователям
+def send_message_to_all(message):
+    global message_to_send
+    message_to_send = message.text
+
+    # Отправляем сообщение другим пользователям
+    for user_id in get_all_users():
+        try:
+            bot.forward_message(user_id, message_to_send)
+        except Exception as e:
+            print(f"Failed to send message to user {user_id}: {str(e)}")
+
+
+@bot.message_handler(func=lambda message: message.text.isdigit())
+def handle_number_message(message):
+    user_id = message.from_user.id
+    increment_user_count(user_id)
+    # Проверяем, было ли сообщение переслано
+    if message.forward_from_chat and message.forward_from_chat.type == 'channel':
+        # Не обрабатываем пересланные сообщения, чтобы избежать циклов
+        return
+
+    channel_id = '-1002046764189'  # Замените '-1002046764189' на актуальный ID вашего канала
+    message_id = int(message.text)
+
+    try:
+        channel_message = bot.forward_message(message.chat.id, channel_id, message_id)
+        if channel_message.text:
+            bot.send_message(message.chat.id, channel_message.text)                
+    except telebot.apihelper.ApiException as e:
+        bot.reply_to(message, f"{message_id}-ID bilan hech qanday film topilmadi!")
+
+def main():
+    bot.polling(none_stop=True)
+
+if __name__ == '__main__':
+    main()
+
